7ff8f6d8400e63e82a52e1fcfe2bed2ebcc64c13
Lauren Brink <laurenfriesbrink@gmail.com>
2016-09-06 12:55:16 -0700

    added more shell files to specs & required_relative

--- farmar.rb
+++ farmar.rb
@@ -2,6 +2,9 @@ require 'csv'
 
 module FarMar
 
-
+require_relative './lib/farmar_market'
+require_relative './lib/farmar_product'
+require_relative './lib/farmar_vendor'
+require_relative './lib/farmar_sale'
 
 end
--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,1 +1,5 @@
 require_relative 'spec_helper'
+require_relative '../lib/farmar_market'
+
+
+describe
